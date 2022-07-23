2d8179fb0a323a17fee8cc54118a25f568b0a25a
Bob Lin <boblinlj@gmail.com>
2022-07-23 19:57:22 -0400

    Updated Run Model Program

--- weekly_financial_statement.py
+++ weekly_financial_statement.py
@@ -2,6 +2,7 @@ from modules.extract_yahoo_financials import YahooFinancial
 import datetime
 from util.transfer_data import UploadData2GCP
 from util.helper_functions import create_log
+from util.send_email import SendEmail
 import sys
 
 
@@ -26,6 +27,20 @@ def main(run_time):
                             loggerFileName=loggerFileName,
                             use_tqdm=True)
     spider.run()
+
+    SendEmail(content=f"""{'-'*80}\n"""
+                      f'Weekly Yahoo Equity Financial Statement job finished for {run_time}\n'
+                      f"""{'-'*80}\n"""
+                      f"""{spider.get_failed_extracts}/{spider.no_of_stock} Failed Extractions. \n"""
+                      f"""The job made {spider.no_of_web_calls} calls through the internet. \n"""
+                      f"""The job made {spider.no_of_db_entries} entries to the database"""
+                      f"""Target Table: yahoo_annual_fundamental\n"""
+                      f"""              yahoo_quarterly_fundamental\n"""
+                      f"""              yahoo_trailing_fundamental\n"""
+                      f"""Target Population: YAHOO_STOCK_ALL\n"""
+                      f"""Log: {loggerFileName}\n"""
+                      f"""{'-'*80}\n""",
+              subject=f'[Do not reply]Weekly Yahoo Equity Financial Statement Job has Finished - {run_time}').send()
 
     sys.stderr.write(f"Extracting Job is Completed, log is produced as {loggerFileName}\n")
     sys.stderr.write(f"{'*' * 80}\n")
